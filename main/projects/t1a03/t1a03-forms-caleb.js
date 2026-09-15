5f0429df9ad6a8a96892ca5ae3bf0b401c965a4f
CM-Kymiira <194865285+Kymiira@users.noreply.github.com>
2026-09-15 13:31:16 -0700

    add t1a03-forms-caleb.js and give ids to elements within t1a03-forms-caleb.html

---
+++ main/projects/t1a03/t1a03-forms-caleb.js
@@ -0,0 +1,28 @@
+// t1a03 scripts
+dButton = document.getElementById('button');
+dBox = document.getElementById('select');
+fName = document.getElementById('fName');
+yBox = document.getElementById('yBox');
+nBox = document.getElementById('nBox');
+
+function doSomething(a, b) {
+    if (a) {
+        dButton.value = "why did you do that?"
+    }
+
+    if (b) {
+        dBox.value = "STOP CLICKING ME"
+    }
+
+    if (c) {
+        fName.value = "Kruschev"
+        yBox.type = "text"
+        yBox.value = "NO ITS NOT"
+    }
+
+    if (d) {
+        fName.value = "Stalin"
+        yBox.type = "text"
+        yBox.value = "w capitalist AMIRITE"
+    }
+}
