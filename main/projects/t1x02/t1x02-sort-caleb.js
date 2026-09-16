48b69f877934185075c6efa445639db055644801
CM-Kymiira <194865285+Kymiira@users.noreply.github.com>
2026-09-16 13:22:53 -0700

    test t1x02-sort-caleb.js additions on t1x02-sort-caleb.html

--- main/projects/t1x02/t1x02-sort-caleb.js
+++ main/projects/t1x02/t1x02-sort-caleb.js
@@ -1,9 +1,28 @@
-x = [7, 3, 8, 1, 9];
-y = 0;
 function doSomething() {
-    for (x[0]; x<[4]; x[]++) {
-        if (x > y) {
-            y = x
-        }
+  
+  let x = {
+    out: document.getElementById('finalOutput'),
+    array: [],
+    temp: 0
+  };
+  
+  let count = ((Math.random() / Math.random()) + (Math.random() / Math.random()) + (Math.random() / Math.random()) + (Math.random() / Math.random()));
+  for (let i = 0; i < count; i++) {
+    x.array[i] = ((Math.random() / Math.random()) + (Math.random() / Math.random()) + (Math.random() / Math.random()) + (Math.random() / Math.random()));
+  }
+
+  function doSomethingElse(arr) {
+    let maxVal = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > maxVal) { maxVal = arr[i];}
     }
+    return maxVal;
+  }
+  x.temp = doSomethingElse(x.array);
+  
+  console.log("Array:", x.array, "Max:", x.temp);
+  x.out.value = x.array;
+    
+  requestAnimationFrame(doSomething);
 }
+doSomething();
