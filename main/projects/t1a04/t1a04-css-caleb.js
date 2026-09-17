4121ce20f43ebd407004aa78feae08b7457f56f3
CM-Kymiira <194865285+Kymiira@users.noreply.github.com>
2026-09-17 11:01:18 -0700

    begin t1a04-css-caleb

---
+++ main/projects/t1a04/t1a04-css-caleb.js
@@ -0,0 +1,13 @@
+const img = document.getElementById('img');
+let direciton = 1;
+let position = 100;
+func move() {
+    position += direction * 5;
+    img.style.left = position + 'px';
+    if (position >= 200) {
+        direction = -1;
+    } else if (position <= -200) {
+        direction 1;
+    } requestAnimationFrame(move);
+}
+move();
