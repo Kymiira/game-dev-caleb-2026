fc63a14dd9c123c9d364fef0ee0a507527eecc77
CM-Kymiira <194865285+Kymiira@users.noreply.github.com>
2026-09-23 11:56:42 -0700

    add setInterval call for post-button press consistant updates every ~1 seconds(?)

--- main/projects/t1a07/t1a07-decisions-caleb.js
+++ main/projects/t1a07/t1a07-decisions-caleb.js
@@ -25,4 +25,5 @@ function doTime() {
     }
     console.log(currentTime);
     output1.textContent = currentTime + 'formatted in /100, to get currentTime in standard please do x/100 = y/60 where x = currentTime and y is your standard time.'
+    setInterval(doTime, 1000);
 }
